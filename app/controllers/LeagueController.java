62c3246b54cffeee841d571bf44d3fa53eefc542
NimendraK <nimendra.2019264@iit.ac.lk>
2020-12-24 01:01:28 +0530

    search done

--- app/controllers/LeagueController.java
+++ app/controllers/LeagueController.java
@@ -19,30 +19,36 @@ public class LeagueController extends Controller {
     private static final Logger logger = LoggerFactory.getLogger("controller");
 
     public Result addRandomMatch() {
-        Match match = PremierLeagueManager.getInstance().addRandomMatch();
-        JsonNode jsonObject = Json.toJson(match);
+        Match result = PremierLeagueManager.getInstance().addRandomMatch();
+        JsonNode jsonObject = Json.toJson(result);
         return created(ApplicationUtil.createResponse(jsonObject, true));
     }
 
     public Result showLeaderboard() {
         List<FootballClub> result = PremierLeagueManager.getInstance().displayLeagueTable();
-        logger.debug("In LeagueController.displayLeagueTable(), result is: {}", result.toString());
+        logger.debug("In LeagueController.showLeaderboard(), result is: {}", result.toString());
         JsonNode jsonObject = Json.toJson(result);
         return ok(ApplicationUtil.createResponse(jsonObject, true));
     }
 
     public Result getSortedLeaderboardData(String sort, String order) {
         List<FootballClub> result = PremierLeagueManager.getInstance().getSortedTableData(sort, order);
-        logger.debug("In LeagueController.displayLeagueTable(), result is: {}", result.toString());
+        logger.debug("In LeagueController.getSortedLeaderboardData(), result is: {}", result.toString());
         JsonNode jsonObject = Json.toJson(result);
         return ok(ApplicationUtil.createResponse(jsonObject, true));
     }
 
     public Result showPlayedMatches() {
         List<Match> result = PremierLeagueManager.getInstance().getPlayedMatches();
-        logger.debug("In LeagueController.displayLeagueTable(), result is: {}", result.toString());
+        logger.debug("In LeagueController.showPlayedMatches(), result is: {}", result.toString());
         JsonNode jsonObject = Json.toJson(result);
         return ok(ApplicationUtil.createResponse(jsonObject, true));
     }
 
+    public Result getSearchedMatch(String date) {
+        List<Match> result = PremierLeagueManager.getInstance().getSearchedMatch(date);
+        logger.debug("In LeagueController.getSearchedMatch(), result is: {}", result.toString());
+        JsonNode jsonObject = Json.toJson(result);
+        return ok(ApplicationUtil.createResponse(jsonObject, true));
+    }
 }
